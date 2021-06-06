4924bf81f551080c9e45e9add7a8f061c66f6ea3
aspiratio <aspiratio-eng@yahoo.co.jp>
2021-06-07 00:55:22 +0900

    3章スネークを自在に動かすところまで終了

---
+++ hooks-snake-game/src/components/Button.jsx
@@ -0,0 +1,13 @@
+const Button = ({ status, onStart, onRestart }) => {
+  return (
+    <div className="button">
+      {status === "gameover" ? (
+        <button onClick={onRestart}>gameover</button>
+      ) : (
+        <button onClick={onStart}>start</button>
+      )}
+    </div>
+  );
+};
+
+export default Button;
